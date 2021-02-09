18c7499348b12e4fe170544f1f34421e0d8766bd
Andrew Borges <andrewborges71@gmail.com>
2021-02-09 20:16:18 -0300

    Iniciando projeto com intuito de exercitar o conhecimento sobre enums e composicao

---
+++ EnumComp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnumComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

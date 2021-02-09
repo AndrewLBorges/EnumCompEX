836adc6340b8bc94b1c354d433c6cf9388da7358
Andrew Borges <andrewborges71@gmail.com>
2021-02-09 20:26:57 -0300

    Criando as classes e enums do projeto

---
+++ EnumComp/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumComp.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+    }
+}
---
+++ EnumComp/Entities/OrderItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumComp.Entities
+{
+    class OrderItem
+    {
+    }
+}

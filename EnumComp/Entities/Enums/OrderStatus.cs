593285419eea4af2a01cdf5918bdbccefa500e34
Andrew Borges <andrewborges71@gmail.com>
2021-02-10 10:45:50 -0300

    Exercício finalizado com modificação no Parse do DateTime para ParseExact()

--- EnumComp/Entities/Enums/OrderStatus.cs
+++ EnumComp/Entities/Enums/OrderStatus.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace EnumComp.Entities.Enums
 {
     enum OrderStatus : int
     {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
     }
 }
--- EnumComp/Program.cs
+++ EnumComp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EnumComp.Entities;
+using EnumComp.Entities.Enums;
+using System;
+using System.Globalization;
 
 namespace EnumComp
 {
@@ -6,7 +9,45 @@ namespace EnumComp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int qtdItems = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= qtdItems; i++)
+            {
+                Console.WriteLine($"Enter #{i} data: ");
+                Console.Write("Product name: ");
+                string pName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product p = new Product(pName, price);
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem item = new OrderItem(quantity, price, p);
+                order.AddItem(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY: ");
+            Console.WriteLine(order);
         }
     }
 }
